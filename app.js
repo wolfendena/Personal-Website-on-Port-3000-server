6dc3ecedb899ba7c9a10d16edc6c719e407361c5
Alex <alexwolfenden@outlook.com>
2019-06-26 19:02:04 +0400

    finished fixing links

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ const app = express();
 //adding middleware
 app.set("view engine", "ejs"); //setting up templating engine
 
+app.get('/Personal', function(req, res) {
+    res.render('Personal.ejs');
+});
+
 app.get('/home', function(req, res) {
-    res.render('home');
+    res.render('home.ejs');
+});
+
+app.get('/Activities', function(req, res) {
+    res.render('Activities.ejs');
+});
+
+app.get('/Portfolio', function(req, res) {
+    res.render('Portfolio.ejs');
 });
 
 app.listen(3000, function(err){
