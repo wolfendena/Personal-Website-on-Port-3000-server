7fc389d67b5bd46648e86259372cf2cde032cc95
Alex <alexwolfenden@outlook.com>
2019-06-26 18:32:16 +0400

    started transferring my website to server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+//adding middleware
+app.set("view engine", "ejs"); //setting up templating engine
+
+app.get('/home', function(req, res) {
+    res.render('home');
+});
+
+app.listen(3000, function(err){
+    if (err){
+        console.log(err) 
+    }
+    console.log('server is live on port 3000')
+});
